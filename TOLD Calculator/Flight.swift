fd82f4395f9e25d377471a9b42034d4857a33fa5
TODD WILSON <toddwilson@todds-mbp.lan>
2017-07-02 08:52:54 -0500

    Added Flight class and Flights data model

---
+++ TOLD Calculator/Flight.swift
@@ -0,0 +1,31 @@
+//
+//  Flight.swift
+//  TOLD Calculator
+//
+//  Created by TODD WILSON on 7/2/17.
+//  Copyright © 2017 TODD WILSON. All rights reserved.
+//
+
+import Foundation
+
+class Flight {
+    
+    var grossWeight: Double = 0.0
+    var wingSweep: Bool = false //wing sweep true = 15 WS or 20 WS
+                                //wing sweep false = 20 WS SEF/SIS OFF
+    var fieldLength: Double = 0.0
+    var pressureAltitude: Double = 0.0
+    var temperature: Double = 0.0   //teperature in ºF
+    var rCR: Double = 0.0
+    
+    var refusalSpeedFactor: Double = 0.0
+    var refusalSpeed: Double = 0.0
+    var decisionSpeed: Double = 0.0
+    var rotateSpeed: Double = 0.0
+    var takeoffSpeed: Double = 0.0
+    var breakCaution: Double = 0.0
+    var breakDanger: Double = 0.0
+    var takeOffDistance: Double = 0.0
+    var cFL: Double = 0.0
+    
+}
