258692d0f2e853bb238de080755b52001a6ae6ac
TODD WILSON <toddwilson@todds-mbp.lan>
2017-07-01 21:27:07 -0500

    Initial Commit

---
+++ TOLD Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TOLD Calculator
+//
+//  Created by TODD WILSON on 7/1/17.
+//  Copyright © 2017 TODD WILSON. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
