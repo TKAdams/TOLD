a1a40e4070fbe2ecb65e564e60b1c7e111f0cd8b
TODD WILSON <toddwilson@todds-mbp.lan>
2017-07-02 23:59:09 -0500

    Adding more to TA

---
+++ TOLD Calculator/TakeoffFactors.swift
@@ -0,0 +1,15 @@
+//
+//  TakeoffFactors.swift
+//  TOLD Calculator
+//
+//  Created by TODD WILSON on 7/2/17.
+//  Copyright © 2017 TODD WILSON. All rights reserved.
+//
+
+import Foundation
+
+class TakeoffFactors {
+    
+    var takeoffTable: [[Int]] = []
+
+}
