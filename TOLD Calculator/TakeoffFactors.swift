193e921a488653fecbecf6ecde1a42f66d9d1bba
TODD WILSON <toddwilson@todds-mbp.lan>
2017-07-03 09:18:21 -0500

    Added CSVReader functionality and improved TakeoffFactor init

--- TOLD Calculator/TakeoffFactors.swift
+++ TOLD Calculator/TakeoffFactors.swift
@@ -10,6 +10,23 @@ import Foundation
 
 class TakeoffFactors {
     
+    //TakeoffTable [[Int]] is an array identical to the IFG lookup table where:
+    //Rows start from index 0 = 120F to 26 = -20F
+    //Note that index 26 should be -15F but they skipped to -20F
     var takeoffTable: [[Int]] = []
+    let tOFFilename: String = "TakeoffFactor"
+    
+    init () {
+        
+        var data = CSVReader.readDataFromCSV(fileName: tOFFilename, fileType: "csv")
+        
+        data = CSVReader.cleanRows(file: data!)
+        let csvStringRows = CSVReader.convertCSVDataToStringArray(data: data!)
+        takeoffTable = CSVReader.convertStringArrayToIntArray(stringData: csvStringRows)
+        
+        print(takeoffTable[1][1])
+        print(takeoffTable[0][0])
+        
+    }
 
 }
