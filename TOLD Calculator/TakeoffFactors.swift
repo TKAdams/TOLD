e7baaddc4cb5d76bb79d756f559e2e6c955147d2
TODD WILSON <toddwilson@todds-mbp.lan>
2017-07-03 09:34:50 -0500

    removed prints

--- TOLD Calculator/TakeoffFactors.swift
+++ TOLD Calculator/TakeoffFactors.swift
@@ -13,6 +13,7 @@ class TakeoffFactors {
     //TakeoffTable [[Int]] is an array identical to the IFG lookup table where:
     //Rows start from index 0 = 120F to 26 = -20F
     //Note that index 26 should be -15F but they skipped to -20F
+    
     var takeoffTable: [[Int]] = []
     let tOFFilename: String = "TakeoffFactor"
     
@@ -23,9 +24,6 @@ class TakeoffFactors {
         data = CSVReader.cleanRows(file: data!)
         let csvStringRows = CSVReader.convertCSVDataToStringArray(data: data!)
         takeoffTable = CSVReader.convertStringArrayToIntArray(stringData: csvStringRows)
-        
-        print(takeoffTable[1][1])
-        print(takeoffTable[0][0])
         
     }
 
